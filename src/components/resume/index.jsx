eac8a6025dd62d933a3bb95421013c6df874d306
chiedozieezenwa <chiedozieezenwa@gmail.com>
2024-09-29 01:33:51 +0100

    feat::About Section

--- src/components/resume/index.jsx
+++ src/components/resume/index.jsx
@@ -2,10 +2,11 @@ import design from "./resume.module.css"
 
 export const Resume = () => {
   return (
-    <div>
-      <section className={design.container}>
-        
+    <div className={design.container}>
+      <section className={design.firstSection}>
+        <p>My Resume</p>
       </section>
+      
       <section></section>
     </div>
   )
